5540b593d2c0a4732ad5d0e72a4ea2056ff80dd8
Jolver Andre Gil Quevedo <jolveragq@ext.massimodutti.com>
2025-01-20 21:47:37 +0100

    part2

--- src/app/components/section-1/section-1.component.ts
+++ src/app/components/section-1/section-1.component.ts
@@ -24,4 +24,30 @@ export class Section1Component implements AfterViewInit {
       const swiper = new Swiper(".mySwiper", {});
     }
   }
+
+  createCalendar() {
+      const title = "Boda de Luis y Karen";
+      const description = "Únete a la celebración de la boda de Luis y Karen.";
+      const location = "Santa María Catedral - Chiclayo";
+      const startDate = "20250215T200000"; // 15 de febrero de 2025 a las 8 PM
+      const endDate = "20250215T230000"; // Finaliza a las 10 PM
+
+      // Crear contenido del archivo .ics
+      const icsContent = `BEGIN:VCALENDAR\nVERSION:2.0\nBEGIN:VEVENT\nSUMMARY:${title}\nDESCRIPTION:${description}\nLOCATION:${location}\nDTSTART:${startDate}\nDTEND:${endDate}\nEND:VEVENT\nEND:VCALENDAR`;
+
+      // Crear archivo Blob para descargar
+      const blob = new Blob([icsContent], { type: 'text/calendar' });
+      const url = URL.createObjectURL(blob);
+
+      // Crear enlace para descargar el archivo
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = 'Boda_de_Luis_y_Karen.ics';
+
+      // Simular clic para iniciar la descarga
+      link.click();
+
+      // Liberar URL
+      URL.revokeObjectURL(url);
+  }
 }
